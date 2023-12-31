c88b8da014077d49a3cfc033c87d922d5a6158f9
Vinnie Moth <vinniemoth@gmail.com>
2023-12-31 16:27:28 -0300

    code refactoring done

--- src/app.js
+++ src/app.js
@@ -2,15 +2,12 @@ const menuBtn = document.getElementById('button');
 const menu = document.getElementById('nav');
 
 menuBtn.addEventListener('click', () => {
-  if (menu.classList.contains('openSidebar')) {
-    menu.classList.toggle('openSidebar');
-    menu.classList.toggle('openSidebar-reverse');
-    menu.classList.toggle('active');
-    console.log('HELLO!');
-  } else if (menu.classList.contains('openSidebar-reverse')) {
-    menu.classList.toggle('openSidebar-reverse');
-    menu.classList.toggle('openSidebar');
-    menu.classList.toggle('active');
-    console.log('HELLO!');
-  }
+  addSidebarAnimation();
 });
+
+const addSidebarAnimation = () => {
+  menu.classList.toggle('openSidebar');
+  menu.classList.toggle('openSidebar-reverse');
+  menu.classList.toggle('active');
+  return;
+};
